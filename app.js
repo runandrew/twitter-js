702e7aa696217218a91371217ede44561a3ced6e
Andrew Garcia <runandrew@gmail.com>
2016-11-07 18:27:51 -0500

    final push for monday

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 //Andrew was here
 const express = require("express");
 const app = express();
+const nunjucks = require("nunjucks");
 
 
 const port = 3000;
@@ -29,3 +30,21 @@ app.get("/", function(req, res, next) {
 app.get("/news", function(req, res, next) {
     res.send("This is the news!");
 });
+
+// let locals = {
+//     title: 'An Example',
+//     people: [
+//         { name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+//     ]
+// };
+
+app.get("/renderTemplate", function(req, res, next) {
+    let people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+    res.render( 'index', {title: 'Hall of Fame', people: people} );
+});
+
+nunjucks.configure("views", { noCache: true});
+app.set("view engine", "html");
+app.engine("html", nunjucks.render);
